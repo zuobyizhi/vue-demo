7ebc26fe0ce40e25f2b0c451af9c08af762f738b
zuobyizhi <zuobyige@sina.cn>
2017-08-21 19:59:25 +0800

    分页

--- src/components/tomato.vue
+++ src/components/tomato.vue
@@ -24,18 +24,21 @@
             <td>{{item.msg}}</td>
             <td>{{item.time}}</td>
             <td>
-              <button v-show='!item.work' @click="setTimerWork(index, true)">启动</button>
-              <button v-show='item.work' @click="setTimerWork(index, false)">暂停</button>
-              <button class="deleteBtn" @click="deleteTimer(index)">删除</button>
+              <button v-show='!item.work' @click="setTimerWork(index, item.id, true)">启动</button>
+              <button v-show='item.work' @click="setTimerWork(index, item.id, false)">暂停</button>
+              <button class="deleteBtn" @click="deleteTimer(item.id)">删除</button>
             </td>
           </tr>
         </tbody>
       </table>
 		</div>
+    <div class="btns">
+      <v-paging :total="total" :current-page='current' :display='display' @pagechange="pagechange"></v-paging>
+    </div>
   </div>
 </template>
 
-<script>
+<script type="es6">
 export default {
   name: 'hello',
   data () {
@@ -43,10 +46,82 @@ export default {
       timer: [],
       msg: '',
       errMsg: '',
-      time: ''
+      time: '',
+      total: 0,     // 记录总条数
+      display: 5,   // 每页显示条数
+      current: 1   // 当前的页数
     }
   },
   methods: {
+    uuid () {
+      var s = [];
+      var hexDigits = "0123456789abcdef";
+      for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+      }
+      s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+      s[8] = s[13] = s[18] = s[23] = "-";
+
+      var uuid = s.join("");
+      return uuid;
+    },
+    getTimerLS (start, end) {
+      let arr = JSON.parse(window.localStorage.getItem('timer'))
+      if (arr instanceof Array && arr.length > 0) {
+        this.timer = arr.slice(start, end)
+        this.total = arr.length
+      } else {
+        this.timer = []
+        this.total = 0
+      }
+    },
+    deleteTimerLS (id) {
+      let arr = JSON.parse(window.localStorage.getItem('timer'))
+      if (arr instanceof Array && arr.length > 0) {
+        for (let i = 0; i < arr.length; ++i) {
+          if (arr[i].id === id) {
+            arr.splice(i, 1)
+            window.localStorage.setItem('timer', JSON.stringify(arr))
+            return
+          }
+        }
+      }
+    },
+    setTimerWordLS (id, bWork) {
+      let arr = JSON.parse(window.localStorage.getItem('timer'))
+      if (arr instanceof Array && arr.length > 0) {
+        for (let i = 0; i < arr.length; ++i) {
+          if (arr[i].id === id) {
+            arr[i].work = bWork
+            window.localStorage.setItem('timer', JSON.stringify(arr))
+            return
+          }
+        }
+      }
+    },
+    setTimerTimeLS (id, time) {
+      let arr = JSON.parse(window.localStorage.getItem('timer'))
+      if (arr instanceof Array && arr.length > 0) {
+        for (let i = 0; i < arr.length; ++i) {
+          if (arr[i].id === id) {
+            arr[i].time = time
+            window.localStorage.setItem('timer', JSON.stringify(arr))
+            return
+          }
+        }
+      }
+    },
+    addTimerLS (timer) {
+      let arr = JSON.parse(window.localStorage.getItem('timer'))
+      if (arr instanceof Array && arr.length > 0) {
+        arr.unshift(timer)
+      } else {
+        arr = []
+        arr.push(timer)
+      }
+      window.localStorage.setItem('timer', JSON.stringify(arr))
+    },
     addTimer () {
       function isNumber (obj) {
         return Number(obj) === +obj
@@ -59,24 +134,39 @@ export default {
         this.errMsg = '内容和倒计时必须非空'
         return
       }
-      this.timer.push({msg: this.msg.trim(), time: this.time.trim(), work: false})
+      // this.timer.push({msg: this.msg.trim(), time: this.time.trim(), work: false})
+      this.addTimerLS({id: this.uuid(), msg: this.msg.trim(), time: this.time.trim(), work: false})
       this.msg = ''
       this.time = ''
       this.errMsg = ''
-      window.localStorage.setItem('timer', JSON.stringify(this.timer))
+      // window.localStorage.setItem('timer', JSON.stringify(this.timer))
+      let currentPage = this.current - 1
+      this.getTimerLS(currentPage * this.display, (currentPage + 1) * this.display)
     },
-    setTimerWork (i, bWork) {
+    setTimerWork (i, id, bWork) {
       this.timer[i].work = bWork
-      window.localStorage.setItem('timer', JSON.stringify(this.timer))
+      // window.localStorage.setItem('timer', JSON.stringify(this.timer))
+      this.setTimerWordLS(id, bWork)
     },
-    deleteTimer (i) {
-      let id = this.$layer.confirm('确认删除？', () => {
-        this.timer.splice(i, 1)
+    deleteTimer (id) {
+      let lid = this.$layer.confirm('确认删除？', () => {
+        this.deleteTimerLS(id)
         this.$layer.msg('删除成功', {})
-        this.$layer.close(id)
-        window.localStorage.setItem('timer', JSON.stringify(this.timer))
+        this.$layer.close(lid)
+        let currentPage = this.current - 1
+        this.getTimerLS(currentPage * this.display, (currentPage + 1) * this.display)
       })
+    },
+    pagechange:function(currentPage){
+      console.log(currentPage);
+      // ajax请求, 向后台发送 currentPage, 来获取对应的数据
+      this.current = currentPage
+      currentPage = this.current - 1
+      this.getTimerLS(currentPage * this.display, (currentPage + 1) * this.display)
     }
+  },
+  components: {
+    'v-paging': require('./paging.vue')
   },
   mounted () {
     let self = this
@@ -85,17 +175,15 @@ export default {
         if (self.timer[i].work) {
           if (Number(self.timer[i].time) > 0) {
             self.timer[i].time -= 1
-            window.localStorage.setItem('timer', JSON.stringify(self.timer))
+            // window.localStorage.setItem('timer', JSON.stringify(self.timer))
+            self.setTimerTimeLS(self.timer[i].id, self.timer[i].time)
           }
         }
       }
     }
     setInterval(fn, 1000)
 
-    let arr = JSON.parse(window.localStorage.getItem('timer'))
-    if (arr instanceof Array && arr.length > 0) {
-      this.timer = arr
-    }
+    this.getTimerLS(0, this.display)
   }
 }
 </script>
